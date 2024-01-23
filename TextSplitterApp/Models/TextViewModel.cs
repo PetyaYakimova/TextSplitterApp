bb0131424157b9a62a771578a918b9ba743991ca
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-23 08:48:33 +0200

    Add project files.

---
+++ TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models
+{
+	/// <summary>
+	/// A model for the text view
+	/// </summary>
+	public class TextViewModel
+	{
+		/// <summary>
+		/// Raw text.
+		/// </summary>
+		[Required]
+		[StringLength(30, MinimumLength = 2)]
+		public string Text { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Split text.
+		/// </summary>
+		public string SplitText { get; set; } = string.Empty;
+	}
+}
